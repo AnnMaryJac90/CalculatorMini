f67be05d246d1c7b724d90c5267f673f318ee4be
Ann Mary <annmaryjac90@gmail.com>
2022-02-07 23:21:07 -0800

    Initial Commit

---
+++ CalculatorMini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorMini
+//
+//  Created by Anil Thomas on 2/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
